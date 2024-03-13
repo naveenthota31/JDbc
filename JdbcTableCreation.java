931b1b66b155b62edc093a666486a97ba1ffe320
JayaDeep <110987107+JayaDeep11@users.noreply.github.com>
2024-03-13 11:07:18 +0530

    TableCreation file created

---
+++ JdbcTableCreation.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class JdbcTableCreation {
+   public static void main(String[] args) throws Exception{
+    // load driver
+    DBConnection.loadDriver();
+    System.out.println("Driver  Loaded...");
+
+    // get connection from DBConnection
+    Connection con = DBConnection.getConnection();
+    System.out.println("Connection  Established....");
+
+    // Create a statement
+    Statement statm = con.createStatement();
+
+    // query
+    String query = "CREATE TABLE students_02 (rollno number(10), name varchar2(10), email varchar2(100))";
+    int n = statm.executeUpdate(query);
+    System.out.println("Value of n is : " + n);
+
+    if (n >= 0) {
+        System.out.println("Students table created successfully !!");
+    }
+    else {
+        System.out.println("Table creation Sucks !!");
+    }
+
+    statm.close();
+    con.close();
+   } 
+}
